4847cce2e453a36c96c71fcadf30530fa6e4fddc
Fernando <fgmoralesc@gmail.com>
2019-03-04 16:59:47 -0300

    Add welcome message

--- bot.js
+++ bot.js
@@ -26,6 +26,7 @@ class EchoBot {
      * @param {TurnContext} on turn context object.
      */
     async onTurn(turnContext) {
+        console.log(turnContext);
         // Handle message activity type. User's responses via text or speech or card interactions flow back to the bot as Message activity.
         // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
@@ -36,6 +37,27 @@ class EchoBot {
             await turnContext.sendActivity(`${ count }: You said "${ turnContext.activity.text }"`);
             // increment and set turn counter.**
             await this.countProperty.set(turnContext, count);
+        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            // Handle ConversationUpdate activity type, which is used to indicates new members add to
+            // the conversation.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+
+            // Do we have any new members added to the conversation?
+            if (turnContext.activity.membersAdded.length !== 0) {
+                // Iterate over all new members added to the conversation
+                for (var idx in turnContext.activity.membersAdded) {
+                    // Greet anyone that was not the target (recipient) of this message
+                    // the 'bot' is the recipient for events from the channel,
+                    // context.activity.membersAdded == context.activity.recipient.Id indicates the
+                    // bot was added to the conversation.
+                    if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
+                        // Welcome user.
+                        // When activity type is "conversationUpdate" and the member joining the conversation is the bot
+                        // we will send its greetings. This will only be sent once, when the Bot joins conversation
+                        await turnContext.sendActivity('Hola, este es un SMS de Twilio. Respondeme porfa');
+                    }
+                }
+            }
         } else {
             // Generic handler for all other activity types.
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
